63c68373bf4c145fe852c408078231b7627a0090
shasikaud <shasikaudayanga@gmail.com>
2023-05-04 11:51:42 +0530

    init

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { AllTasks } from '../features/allTasks/AllTasks.js';
+
+export function App() {
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <AllTasks/>
+    </div>
+  )
+}
---
+++ src/features/allTasks/AllTasks.js
@@ -0,0 +1,53 @@
+import  React,  { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { 
+  addTask, 
+  markTask,
+  deleteTask,
+  selectAllTasks 
+} from './allTasksSlice';
+import { Task } from '../Task.js/Task';
+
+export const AllTasks = () => {
+
+    const tasks = useSelector(selectAllTasks);
+    const dispatch = useDispatch();
+
+    const [inputValue, setInputValue] = useState('enter text here!');
+
+    console.log('rendering AllTasks');
+
+    const markTaskHandler = (task) => {
+      dispatch(markTask(task));
+    }
+
+    const deleteTaskHandler = (task) => {
+      dispatch(deleteTask(task));
+    }
+
+    return (
+        <div>
+          <div>
+            {tasks.map( task => (
+                  <Task 
+                       key={task.id} 
+                       task={task} 
+                       markTaskHandler={markTaskHandler}
+                       deleteTaskHandler={deleteTaskHandler}
+                  />
+            ))}
+          </div>
+
+          <input value={inputValue}
+                  onChange={ e => setInputValue(e.target.value) }
+          />
+
+          <button onClick={ 
+            () => { 
+              dispatch(addTask({ id: Date.now(), data: inputValue, isSelected: false }))
+              setInputValue('')
+           } }>ADD</button>
+
+        </div>
+    );
+}
